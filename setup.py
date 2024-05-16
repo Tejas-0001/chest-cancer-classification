a8c3b9d6365e4d286e3c906e29ec49a3d8ddec59
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-05-16 15:35:09 +0530

    installing local package and requirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+__version__ = "0.0.0"
+
+REPO_NAME = "chest-cancer-classification"
+AUTHOR_USER_NAME = "Tejas-0001"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "tejasraj0001@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
